d1347e50ad046272c9cfc3032db922cd2a2f0fbe
Shyam9583 <strikerallin1@gmail.com>
2020-11-12 17:10:01 +0530

    added loading spinner to orders route

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,32 +1,43 @@
 import React, { Component } from "react";
 import axios from "../../axios-orders";
 import Order from "./Order/Order";
+import Spinner from "../../UI/Spinner/Spinner";
 
 class Orders extends Component {
   state = {
     orders: [],
+    loading: false,
   };
 
   componentDidMount() {
+    this.setState({ loading: true });
     axios
       .get("orders.json")
       .then((result) => {
+        this.setState({ loading: false });
         const orders = [];
         for (let key in result.data) {
           orders.push({ id: key, ...result.data[key] });
         }
         this.setState({ orders: orders });
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        this.setState({ loading: false });
+        console.log(error);
+      });
   }
   render() {
-    const orders = this.state.orders.map((order) => (
-      <Order
-        ingredients={order.ingredients}
-        price={order.price}
-        key={order.id}
-      />
-    ));
+    const orders = this.state.loading ? (
+      <Spinner />
+    ) : (
+      this.state.orders.map((order) => (
+        <Order
+          ingredients={order.ingredients}
+          price={order.price}
+          key={order.id}
+        />
+      ))
+    );
     return <div>{orders}</div>;
   }
 }
